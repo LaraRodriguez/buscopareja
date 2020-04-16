0a795d79d38c0f7afdb5052c270a78b979591142
LilithRelish <laragoblin@gmail.com>
2020-04-16 09:30:06 +0200

    separations...

--- codigofuente.sql
+++ codigofuente.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE IF NOT EXISTS Buscopareja CHARACTER SET utf8 COLLATE utf8_spanish_ci;
-/*TODO: tabla de usuarios, con toda la información de la persona
-        tabla de citas, esto sigue fallando, mirar como meter dos personas diferentes
-        tabla de preferencias, esta no da para mucho, asi que ni me lo planteo */
+/*TODO: table users, information schema
+        table dates, this doesn't work...
+        table preferences, this works, don't touch!!!*/
 use Buscopareja;
-/*Creamos la base de datos y cambiamos a ella para poder editarla*/
+/*Change databaseeeee*/
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
 CREATE TABLE IF NOT EXISTS usuarios(
 cod_usuario INT AUTO_INCREMENT, 
 nombre VARCHAR(20), 
@@ -22,6 +23,8 @@ foto VARCHAR(10),
 PRIMARY KEY(cod_usuario, nombre))
 ENGINE=InnoDB;
 /*Tabla usuarios*/
+/*----------------------------------------------------------------------------------------------------------------------------------------*/
+/*DON'T FUCKING TOUCH THIS, THIS WORKS AND THIS IS HOW IT WILL REMAIN*/
 CREATE TABLE IF NOT EXISTS preferencias(
 cod_usuario INT,
 nombre VARCHAR(20),
@@ -30,7 +33,8 @@ fecha DATE,
 obligacion SET("Si", "No"),
 CONSTRAINT FK_USUARIO_PREF FOREIGN KEY (cod_usuario, nombre) REFERENCES usuarios (cod_usuario, nombre) ON UPDATE CASCADE ON DELETE CASCADE)
 ENGINE=InnoDB;
-/*Tabla preferencias*/
+/*DON'TTTTTTT TOUUUUUUUCH*/
+/*-----------------------------------------------------------------------------------------------------------------------------------------*/
 CREATE TABLE IF NOT EXISTS citas(
 cod_cita INT AUTO_INCREMENT PRIMARY KEY,
 persona1 VARCHAR(20),
@@ -44,5 +48,5 @@ PRIMARY KEY (persona1, cod_persona1),
 CONSTRAINT FK_ASISTENTE1 FOREIGN KEY (persona1, cod_persona1) REFERENCES usuarios (nombre, cod_usuario))
 ENGINE=InnoDB;
 /*CONSTRAINT FK_ASISTENTE2 FOREIGN KEY (persona2, cod_persona2) REFERENCES usuarios (nombre, cod_usuario))*/
-
 /*TODO:No estoy muy segura, ¿esto funciona? Esto no funciona, lo confirmamos*/
+/*-----------------------------------------------------------------------------------------------------------------------------------------*/
